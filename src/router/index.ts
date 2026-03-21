f2b3c00a8904b19f1403e369d708ecc2aaa1c6bf
week-book <danilsinickij9@gmail.com>
2026-03-21 07:54:29 +0200

    feat: add about-page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Posts from '../pages/Posts.vue';
 import PostView from '../pages/PostView.vue';
+import About from '../pages/About.vue';
 
 const routes = [
   { path: '/', component: Posts },
   { path: '/posts/:slug', component: PostView, props: true },
+  { path: '/about', component: About },
 ];
 
 export default createRouter({
